5d9f1f56dba5871a0662f74e3110c89b29f54eb3
retsa-exe <asterlin7890@gmail.com>
2025-09-12 10:29:52 -0400

    Complete Week 2 Task 1

--- Assets/Week 2 Class/Scripts/Controllers/Player.cs
+++ Assets/Week 2 Class/Scripts/Controllers/Player.cs
@@ -9,6 +9,9 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    //bomb trail variables
+    public float bombTrailSpacing;
+    public int numberOfTrailBombs;
 
     void Update()
     {
@@ -27,6 +30,12 @@ public class Player : MonoBehaviour
         {
             transform.position = transform.position + (Vector3)directionToMove.normalized * speed;
         }
+
+        //instantiate bomb trail when press T
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
     }
 
     public void SpawnBombAtOffset (Vector2 inOffset)
@@ -34,4 +43,16 @@ public class Player : MonoBehaviour
         Instantiate(bombPrefab, inOffset + (Vector2)transform.position, Quaternion.identity);
     }
 
+    //instantiate a series of bomb behinde player
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
+    {
+        for (int i = 0; i < inNumberOfBombs; i++)
+        {
+            //calculate the place of bomb
+            Vector2 placeOfBomb = (Vector2)transform.position - new Vector2(0, inBombSpacing) * (i + 1);
+
+            //place the bomb at the right position
+            Instantiate(bombPrefab, placeOfBomb, Quaternion.identity);
+        }
+    }
 }
