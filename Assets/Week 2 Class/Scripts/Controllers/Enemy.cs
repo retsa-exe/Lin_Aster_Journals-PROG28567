8b37354d0943fc3fd32c2124dd4e1220bab48376
retsa-exe <asterlin7890@gmail.com>
2025-09-20 09:53:07 -0400

    Completed Week 3 Task 2

--- Assets/Week 2 Class/Scripts/Controllers/Enemy.cs
+++ Assets/Week 2 Class/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,57 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
-    private void Update()
+    public Vector3 direction;
+    Vector2 screenPos;
+    public float t;
+    public AnimationCurve animationCurve;
+    public float animationTime = 1f;
+    public Vector3 pos;
+
+    float previousY;
+
+    private void Start()
     {
+        direction = Vector3.zero;
+        direction.x = Random.Range(2f, 5f);
+
+        t = 0;
+        //record the previous y
+        previousY = transform.position.y;
     }
 
+    private void Update()
+    {
+        EnemyMovement();
+    }
+
+    public void EnemyMovement()
+    {
+        pos = transform.position;
+
+        //calculate the screen position
+        screenPos = Camera.main.WorldToScreenPoint(pos);
+        //make the enemy bounce in the screen
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        {
+            direction.x *= -1;
+        }
+        
+        //set the timer
+        t += Time.deltaTime;
+
+        //reset the timer when exceed
+        if (t > animationTime)
+        {
+            t = 0;
+        }
+
+        //calculate y off set
+        float yOffset = animationCurve.Evaluate(t);
+
+        //move the enemy
+        pos.x += direction.x * Time.deltaTime;
+        pos.y = previousY + yOffset;
+        transform.position = pos;
+    }
 }
