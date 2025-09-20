42c4e71529f4eb3b63a2b43f813048c92ade3db3
retsa-exe <asterlin7890@gmail.com>
2025-09-20 12:48:14 -0400

    Complete Week 3 Task 4

--- Assets/Week 2 Class/Scripts/Controllers/Stars.cs
+++ Assets/Week 2 Class/Scripts/Controllers/Stars.cs
@@ -7,24 +7,44 @@ public class Stars : MonoBehaviour
     public List<Transform> starTransforms;
     public float drawingTime;
 
+    public float t;
+
+    int i = 0;
 
     // Update is called once per frame
     void Update()
     {
-        //DrawConstellation();
+        DrawConstellation();
+
     }
 
     public void DrawConstellation()
     {
-        for (int i = 0; i < starTransforms.Count; i++)
+        //start the timer
+        t += Time.deltaTime;
+
+        //calculate the ratio
+        float ratio = t / drawingTime;
+
+        //calculate the point
+        Vector3 start = starTransforms[i].position;
+        Vector3 end = Vector3.Lerp(starTransforms[i].position, starTransforms[i + 1].position, ratio);
+
+        //draw the line
+        Debug.DrawLine(start, end, Color.white);
+
+        //restart the timer when reach the limit, also change the point
+        if (t > drawingTime)
         {
-            DrawLine(starTransforms[i], starTransforms[i + 1]);
+            t = 0;
+
+            //go to the next line
+            i++;
+
+            if (i > starTransforms.Count - 2)
+            {
+                i = 0;
+            }
         }
-
-    }
-
-    public void DrawLine(Transform a, Transform b)
-    {
-        //Debug.DrawLine(a.position, b.position, Color.white);
     }
 }
