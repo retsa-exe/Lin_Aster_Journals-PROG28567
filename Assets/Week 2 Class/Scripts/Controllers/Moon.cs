20e84abd5f8b75e7ee996c88dbd0a26a54eaa87d
retsa-exe <asterlin7890@gmail.com>
2025-09-24 23:09:52 -0400

    Completed Week 4 Task 3

--- Assets/Week 2 Class/Scripts/Controllers/Moon.cs
+++ Assets/Week 2 Class/Scripts/Controllers/Moon.cs
@@ -6,15 +6,36 @@ public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
+    //orbiting variables
+    public float moonRadius;
+    public float moonSpeed;
+    float t;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set the moon to the start place
+        transform.position = planetTransform.position + Vector3.right * moonRadius;
     }
 
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(moonRadius, moonSpeed, planetTransform);
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        //start timer
+        t += Time.deltaTime;
+
+        float vectorRadius = speed * t;
+
+        float x = Mathf.Cos(vectorRadius);
+        float y = Mathf.Sin(vectorRadius);
+
+        Vector3 moonPosition = new Vector3(x, y, 0) * radius + target.position;
+
+        transform.position = moonPosition;
     }
 }
